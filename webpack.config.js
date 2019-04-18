ae4e3ee0a6adadece323df92cc591700214e4729
Daiki Hashimoto <daisea3e1203@gmail.com>
2019-04-18 21:59:28 +0900

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html"
+    })
+  ]
 };
