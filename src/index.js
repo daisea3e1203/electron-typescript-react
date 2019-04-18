8b4ddf83041706af525fd83ac74b57209ec16393
Daiki Hashimoto <daisea3e1203@gmail.com>
2019-04-18 22:13:11 +0900

    add images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "../static/heimenkousei.jpg";
 
 function component() {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   },
