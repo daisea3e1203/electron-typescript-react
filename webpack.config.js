83b7373a6ce21d96bd20c78ad0329575c91ddbf4
Daiki Hashimoto <daisea3e1203@gmail.com>
2019-04-19 08:55:54 +0900

    clean dist before buidling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,6 +34,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./static/index.html"
     })
