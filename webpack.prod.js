5f3ed20a7a5f0b11afc2055bc2de5d71ee562cf0
Daiki Hashimoto <daisea3e1203@gmail.com>
2019-04-20 20:31:06 +0900

    add soure map to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,9 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production"
+  mode: "production",
+  // webpack told me I should have a source-map
+  // even in production build
+  // TODO minify css. maybe have a look at https://webpack.js.org/guides/production/
+  devtool: "source-map"
 });
